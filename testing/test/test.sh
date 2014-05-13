e4e8e87c5a0bf517a9392de8a30ba415df3a4fb5
jbandela <jbandela@gmail.com>
2014-05-13 15:30:30 -0600

    tweaks to linux test script

--- testing/test/test.sh
+++ testing/test/test.sh
@@ -20,7 +20,7 @@ rm unit_test_exe
 
 echo "Building g++ .so"
 
-g++ -std=c++11 ../../implementation/cppcomponents_concurrency.cpp -I ../../../cppcomponents -I ../../ -shared -o cppcomponents_concurrency.so -lboost_coroutine -fPIC -fvisibility=hidden  -lboost_system -lboost_context -Wl,--no-undefined
+g++ -std=c++11 ../../implementation/cppcomponents_concurrency.cpp -I ../../../cppcomponents -I ../../ -shared -o cppcomponents_concurrency.so -lboost_coroutine -fPIC -fvisibility=hidden  -lboost_system -lboost_context -Wl,--no-undefined -O2
 
 echo "Building clang executable"
 
@@ -30,7 +30,5 @@ echo "Running clang++(exe) with g++(so)"
 
 ./unit_test_exe --gtest_print_time=0
 
-rm *.so
 
-rm unit_test_exe
 
